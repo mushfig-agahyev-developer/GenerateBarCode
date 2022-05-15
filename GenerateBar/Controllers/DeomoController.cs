dc5767993f176ee5f2d208508cdd372ce4cc33df
mushfig-agahyev-developer <webprogrammer.readonly@gmail.com>
2022-05-15 13:02:28 +0400

    Add project files.

---
+++ GenerateBar/Controllers/DeomoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenerateBar.Controllers
+{
+    [Route("Demo")]
+    public class DeomoController : Controller
+    {
+        [Route("~/")]
+        [Route("")]
+        [Route("Index")]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("generate")]
+        public IActionResult Generate(string productId)
+        {
+            ViewBag.productId = productId;
+            return View("Index");
+        }
+
+    }
+}
---
+++ GenerateBar/Tags/BrCodeTagHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using ZXing.QrCode;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace GenerateBar
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("brcode")]
+    public class BrCodeTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var content = context.AllAttributes["content"].Value.ToString();
+            var width = int.Parse(context.AllAttributes["width"].Value.ToString());
+            var height = int.Parse(context.AllAttributes["height"].Value.ToString());
+
+           
+           
+
+            var barcodeWriterPixelData = new ZXing.BarcodeWriterPixelData
+            {
+                Format = ZXing.BarcodeFormat.CODE_128,
+                Options = new QrCodeEncodingOptions
+                {
+                    Height = height,
+                    Width = width,
+                    Margin = 0
+                }
+            };
+            var pixelData = barcodeWriterPixelData.Write(content);
+             
+           // string path = "C:/Users/Code/source/repos/GenerateBar/GenerateBar/wwwroot/image/brcode";
+            using (var bitmap = new Bitmap(pixelData.Width, pixelData.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb))
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                   
+
+                    var bitmapData = bitmap.LockBits(new Rectangle(0, 0, pixelData.Width, pixelData.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+                    try
+                    {
+                        System.Runtime.InteropServices.Marshal.Copy(pixelData.Pixels, 0, bitmapData.Scan0, pixelData.Pixels.Length);
+                    }
+                    finally
+                    {
+                        bitmap.UnlockBits(bitmapData);
+                    }
+
+                    bitmap.Save(memoryStream, ImageFormat.Png);
+                    output.TagName = "img";
+                    output.Attributes.Clear();
+                    output.Attributes.Add("width", width);
+                    output.Attributes.Add("height", height);
+                    output.Attributes.Add("src", String.Format("data:image/png;base64,{0}", Convert.ToBase64String(memoryStream.ToArray())));
+
+                    string A = Convert.ToBase64String(memoryStream.ToArray());//test ucun
+
+                    byte[] bytes = memoryStream.ToArray();              
+                    //this is a simple white background image
+                    var myfilename = string.Format(@"{0}", Guid.NewGuid());
+                    //Generate unique filename
+                    string filepath = @"C:\Users\Code\source\repos\GenerateBar\GenerateBar\wwwroot\image\brcode\" + myfilename + ".png";
+                    //var bytess = Convert.FromBase64String(bytes);
+                    using (var imageFile = new FileStream(filepath, FileMode.Create))
+                    {
+                        imageFile.Write(bytes, 0, bytes.Length);
+                        imageFile.Flush();
+                    }
+                }
+      
+            }
+
+        }
+    }
+}
